76171992328ed9841f5816094b4ef11d6ab494e2
Rubin Simons <me@rubin55.org>
2022-10-18 23:36:19 +0200

    Heavily refactored, based on input events, added toggle, mute and unmute direct commands.

--- src/tpmmld/application.py
+++ src/tpmmld/application.py
@@ -1,11 +1,14 @@
 import logging
 from argparse import ArgumentParser
+from typing import Dict, Any
+
 from daemon import DaemonContext
 from os import getgid, getuid, nice
 from subprocess import Popen, PIPE, STDOUT
 from sys import stderr, stdout, version_info
 from time import sleep
 from pulsectl import Pulse
+from evdev import InputDevice, categorize, ecodes, list_devices
 
 from .version import __version__
 
@@ -47,6 +50,21 @@ class Application:
                             default=False,
                             help=f'show list of sources, with index numbers',
                             action='store_true')
+        parser.add_argument('-t', '--toggle',
+                            dest='TOGGLE',
+                            default=False,
+                            help=f'toggle mute/unmute of all sources directly and exit',
+                            action='store_true')
+        parser.add_argument('-m', '--mute',
+                            dest='MUTE',
+                            default=False,
+                            help=f'mute all sources directly and exit',
+                            action='store_true')
+        parser.add_argument('-u', '--unmute',
+                            dest='UNMUTE',
+                            default=False,
+                            help=f'unmute all sources directly and exit',
+                            action='store_true')
         parser.add_argument('-l', '--loglevel',
                             dest='LOGLEVEL',
                             default="info",
@@ -55,7 +73,6 @@ class Application:
                             type=str.upper)
         parser.add_argument('-v', '--version', action='version',
                             version=f"{program} {__version__}")
-
         # Initialized args.
         self.args = parser.parse_args()
 
@@ -80,95 +97,108 @@ class Application:
         # Do we want to daemonize?
         self.daemonize = self.args.DAEMONIZE
 
-        # The led state file.
-        self.led_state_file = '/sys/class/leds/platform::micmute/brightness'
+    def _set_led(self, send_int: int) -> int:
+        send_chars = str(send_int) + '\n'
+        led_state_file = '/sys/class/leds/platform::micmute/brightness'
+        process = Popen(['sudo', 'tee', led_state_file], stdout=PIPE, stdin=PIPE, stderr=STDOUT, text=True)
+        log.debug(f"Sending {send_int} to {led_state_file}")
+        result = process.communicate(input=send_chars)[0]
+        log.debug(f"Process: {process}, Result: {result}")
+        return process.returncode
 
-    def loop(self):
-        # Initialize pulse object.
-        pulse = Pulse(connect=False)
-
-        # Connect to Pulse instance.
-        pulse.connect(wait=True)
-
-        # Put Pulse sources in an index.
+    def _get_sources(self, pulse: Pulse) -> Dict[int, Any]:
         sources = {}
         for source in pulse.source_list():
             sources.update({source.index: source})
+        return sources
 
-        # Show list of sources.
-        if self.args.SOURCES:
+    def _get_volumes(self, pulse: Pulse) -> Dict[int, Any]:
+        volumes = {}
+        for source in self._get_sources(pulse).values():
+            volumes.update({source.index: pulse.volume_get_all_chans(source)})
+        return volumes
 
-            for source in sources.values():
-                if source.mute == 1:
-                    state = 'muted'
-                else:
-                    state = 'unmuted'
+    def mute(self, pulse: Pulse) -> int:
+        for source in self._get_sources(pulse).values():
+            log.info(f"Muting source {source.index}")
+            pulse.volume_set_all_chans(source, self._get_volumes(pulse)[int(source.index)])
+            pulse.mute(source, mute=True)
 
-                print(f"Source index {source.index}: \"{source.description}\" ({state})")
-            exit(0)
+        log.info(f"Turning on mute-mic indicator led")
+        return self._set_led(1)
 
-        # Store current volume levels.
-        volume_memory = {}
-        for source in sources.values():
-            volume_memory.update({source.index: pulse.volume_get_all_chans(source)})
+    def unmute(self, pulse) -> int:
+        for source in self._get_sources(pulse).values():
+            log.info(f"Unmuting source {source.index}")
+            pulse.volume_set_all_chans(source, self._get_volumes(pulse)[int(source.index)])
+            pulse.mute(source, mute=False)
 
-        # Initial mute state.
-        initial_mute_counter = 0
-        for source in sources.values():
-            initial_mute_counter += source.mute
+        log.info(f"Turning off mute-mic indicator led")
+        return self._set_led(0)
 
-        # Previous mute state.
-        previous_mute_counter = initial_mute_counter
+    def sources(self, pulse) -> int:
+        for source in self._get_sources(pulse).values():
+            if source.mute == 1:
+                state = 'muted'
+            else:
+                state = 'unmuted'
 
-        # Loopsy.
-        initial_run = True
-        while True:
-            updated_mute_counter = 0
-            for source in sources.values():
-                updated_mute_counter += pulse.get_source_by_name(source.name).mute
+            print(f"Source index {source.index}: \"{source.description}\" ({state})")
 
-            if initial_run or previous_mute_counter > updated_mute_counter:
-                log.info(f"Mute counter went down, from {previous_mute_counter} to {updated_mute_counter}, unmuting all input sources:")
-                for source in sources.values():
-                    log.info(f"Unmuting source {source.index}")
-                    pulse.volume_set_all_chans(source, volume_memory[int(source.index)])
-                    pulse.mute(source, mute=False)
+        return 0
 
-                log.info(f"Turning off mute-mic indicator led")
-                # if path.exists(self.led_state_file) and path.isfile(self.led_state_file):
-                #     with open(self.led_state_file, 'w') as f:
-                #         f.write('0')
-                p = Popen(['sudo', 'tee', self.led_state_file], stdout=PIPE, stdin=PIPE, stderr=STDOUT).communicate(input=b'0\n')
+    def toggle(self, pulse) -> int:
+        mute_counter = 0
+        for source in self._get_sources(pulse).values():
+            mute_counter += source.mute
 
-                # And set previous mute counter to updated mute counter.
-                previous_mute_counter = updated_mute_counter
+        if mute_counter > 0:
+            return self.unmute(pulse)
+        else:
+            return self.mute(pulse)
 
-            elif previous_mute_counter < updated_mute_counter:
-                log.info(f"Mute counter went up, from {previous_mute_counter} to {updated_mute_counter}, muting all input sources:")
-                for source in sources.values():
-                    log.info(f"Muting source {source.index}")
-                    pulse.volume_set_all_chans(source, volume_memory[int(source.index)])
-                    pulse.mute(source, mute=True)
+    def loop(self, pulse):
+        # Make sure we're connected and wait for it.
+        pulse.connect(wait=True)
 
-                log.info(f"Turning on mute-mic indicator led")
-                # if path.exists(self.led_state_file) and path.isfile(self.led_state_file):
-                #     with open(self.led_state_file, 'w') as f:
-                #         f.write('1')
-                p = Popen(['sudo', 'tee', self.led_state_file], stdout=PIPE, stdin=PIPE, stderr=STDOUT).communicate(input=b'1\n')
+        # Determine event input device.
+        thinkpad_evdev_name = "ThinkPad Extra Buttons"
+        thinkpad_micmute_event_code = 248
+        devices = [InputDevice(path) for path in list_devices()]
+        for device in devices:
+            if device.name == thinkpad_evdev_name:
+                log.info(f"Monitoring keypresses from {device.path} ({device.name})..")
+                for event in device.read_loop():
+                    if event.type == ecodes.EV_KEY and event.code == thinkpad_micmute_event_code and event.value == 1:
+                        self.toggle(pulse)
 
-                # And set previous mute counter to updated mute counter.
-                previous_mute_counter = updated_mute_counter
-
-            # Initial run only happens once.
-            initial_run = False
-
-            # Pause before re-evaluating.
-            sleep(1)
+        # Error out if we did not find a thinkpad extra buttons input.
+        if 'watch' not in locals():
+            log.error(f"Could not find an event input device matching \"{thinkpad_evdev_name}\"")
+            exit(1)
 
     def run(self):
+        # Show list of sources.
+        if self.args.SOURCES:
+            exit(self.sources(Pulse()))
+
+        # Toggle mute/unmute directly.
+        if self.args.TOGGLE:
+            exit(self.toggle(Pulse()))
+
+        # Mute directly.
+        if self.args.MUTE:
+            exit(self.mute(Pulse()))
+
+        # Unmute directly.
+        if self.args.UNMUTE:
+            exit(self.unmute(Pulse()))
+
         # Run main loop, backgrounded or foregrounded.
         if self.daemonize:
+            log.info("Running in background (daemon) mode")
             with DaemonContext(uid=getuid(), gid=getgid(), stdout=stdout, stderr=stderr):
-                self.loop()
+                self.loop(Pulse(connect=False))
         else:
-            self.loop()
+            log.info("Running in foreground mode")
+            self.loop(Pulse(connect=False))
